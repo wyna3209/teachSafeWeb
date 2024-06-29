d588d2280ebd6da30d27483d439c8da0d5e547cc
wyna3209 <skdnjsduq@naver.com>
2024-06-29 23:34:29 +0900

    commit48

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "54173379"
+    serviceWorkerVersion: "3052299698"
   }
 });
