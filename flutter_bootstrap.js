bc02a4b6349cda1ff566f033f1014f27aeb3a48c
wyna3209 <skdnjsduq@naver.com>
2025-04-13 22:05:43 +0900

    google_sign_in

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2903151706"
+    serviceWorkerVersion: "1535859402"
   }
 });
